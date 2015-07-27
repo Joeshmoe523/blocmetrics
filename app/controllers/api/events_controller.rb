f49135543bcd1db9d19681536be3782ab3a5e878
Joeshmoe523 <joeshmoe523@gmail.com>
2015-07-26 21:41:28 -0700

    API Controller and CORS completed

---
+++ app/controllers/api/events_controller.rb
@@ -0,0 +1,33 @@
+class API::EventsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_filter :set_access_control_headers
+
+  def create
+    registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
+    puts registered_application.inspect
+    if registered_application == nil
+      render json: "Unregistered application", status: :unprocessable_entity
+    elsif registered_application
+      registered_application.events.create(event_params)
+      render json: @event, status: :created
+    end
+  end
+
+
+   def set_access_control_headers
+ # #1
+     headers['Access-Control-Allow-Origin'] = '*'
+ # #2
+     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+ # #3
+     headers['Access-Control-Allow-Headers'] = 'Content-Type'
+   end
+
+
+  private
+
+  def event_params
+    params.permit(:event_name)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  namespace :api, defaults: { format: :json } do 
+    match 'create_event', to:"events#create", via: [:options]
+    resources :events, only: [:create]
+  end
+
   resources :registered_applications
   devise_for :users
   resources :welcome, only: [:index, :about]
