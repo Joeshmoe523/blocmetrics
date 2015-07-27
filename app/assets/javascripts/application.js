29a5f4a385ffe08cc196ffc3d038960c30b2ad10
Joeshmoe523 <joeshmoe523@gmail.com>
2015-07-27 11:01:49 -0700

    Client Side JS added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,23 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+
+var blocmetrics = {}
+blocmetrics.report = function(eventName){
+  // This function tracks when the user does an event
+  // 
+
+  var event = { event_name: eventName };
+
+  console.debug(event)
+
+  var request = new XMLHttpRequest();
+  request.open("POST", "http://localhost:3000/api/events", true);
+  request.setRequestHeader('Content-Type', 'application/json');
+
+  request.onreadystatechange = function() {
+    // #2
+  };
+
+  request.send(JSON.stringify(event));
+};
